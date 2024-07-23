c5172152dd88a01cda8bc0c40963fb81f01008a1
Alexis Ortega <alexis.oc3@gmail.com>
2024-07-23 17:30:52 -0600

    Annotate Roadmap model

--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: roadmaps
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Roadmap < ApplicationRecord
 end
