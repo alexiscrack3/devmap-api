bd8bb7e65980d7073a929dbda2e5b33356cf97f8
Alexis Ortega <alexis.oc3@gmail.com>
2024-08-04 15:44:26 -0600

    Test roadmap model

--- test/models/roadmap_test.rb
+++ test/models/roadmap_test.rb
@@ -3,7 +3,21 @@
 require "test_helper"
 
 class RoadmapTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save roadmap with title" do
+    roadmap = Roadmap.new(title: Faker::ProgrammingLanguage.name)
+
+    roadmap.save!
+
+    assert roadmap.persisted?
+  end
+
+  test "should not save roadmap without title" do
+    roadmap = Roadmap.new(title: nil)
+
+    assert_raises ActiveRecord::RecordInvalid do
+      roadmap.save!
+
+      refute roadmap.persisted?
+    end
+  end
 end
