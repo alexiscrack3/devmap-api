6608d77175c84841beacb7163066a872ec37f21c
Alexis Ortega <alexis.oc3@gmail.com>
2024-08-21 17:27:20 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -8,3 +9,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Roadmap.destroy_all
+
+roadmaps = [
+  "Frontend",
+  "Backend",
+  "DevOps",
+  "Full Stack",
+  "Android",
+  "iOS",
+  "Ruby on Rails",
+]
+
+roadmaps.each do |roadmap|
+  Roadmap.create({ title: roadmap })
+end
