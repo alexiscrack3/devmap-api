0fcc31029954f25e52f4405926d1f831cce2c33c
Alexis Ortega <alexis.oc3@gmail.com>
2024-08-22 12:55:06 -0600

    Annotate section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: sections
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  step_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Section < ApplicationRecord
   belongs_to :step
 end
