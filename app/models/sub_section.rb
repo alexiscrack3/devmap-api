6c636c35bc7852ec0b843bd207db5d1bc6fb90d8
Alexis Ortega <alexis.oc3@gmail.com>
2024-10-10 22:43:19 -0700

    Annotate sub section model

--- app/models/sub_section.rb
+++ app/models/sub_section.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: sub_sections
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  section_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class SubSection < ApplicationRecord
   belongs_to :section
 end
