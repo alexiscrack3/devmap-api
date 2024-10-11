abd92460a0f9e870ab70b5b33b4b9e149ed4b4f3
Alexis Ortega <alexis.oc3@gmail.com>
2024-10-10 23:33:19 -0700

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_11_053718) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_062726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_053718) do
     t.datetime("created_at", null: false)
     t.datetime("updated_at", null: false)
     t.index(["section_id"], name: "index_sub_sections_on_section_id")
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string("email")
+    t.datetime("created_at", null: false)
+    t.datetime("updated_at", null: false)
   end
 
   add_foreign_key "sections", "steps"
